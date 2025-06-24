ea3a4282c579ba5d17c29e00cb4c8aa99ad50d43
yumoku <150928992+yumoku@users.noreply.github.com>
2025-06-24 10:01:00 -0400

    Add some new exercises for lesson two

--- Lesson2_Problem_Sets/Stamps.py
+++ Lesson2_Problem_Sets/Stamps.py
@@ -11,15 +11,18 @@
 # (No fair for USians to just say use a "Forever" stamp and be done with it!)
 #
 
-def stamps():
+def stamps(price):
     # Your code here
+    five_cents = price//5
+    two_cents = (price - 5*five_cents)//2
+    one_cents = price - 5*five_cents - 2*two_cents
+    return (five_cents, two_cents, one_cents)
 
-
-print stamps(8)
+print (stamps(8))
 #>>> (1, 1, 1)  # one 5p stamp, one 2p stamp and one 1p stamp
-print stamps(5)
+print (stamps(5))
 #>>> (1, 0, 0)  # one 5p stamp, no 2p stamps and no 1p stamps
-print stamps(29)
+print (stamps(29))
 #>>> (5, 2, 0)  # five 5p stamps, two 2p stamps and no 1p stamps
-print stamps(0)
+print (stamps(0))
 #>>> (0, 0, 0) # no 5p stamps, no 2p stamps and no 1p stamps
