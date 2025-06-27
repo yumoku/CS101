836068256f46adf31b49c97d0421a39c28a91cf6
yumoku <150928992+yumoku@users.noreply.github.com>
2025-06-27 09:53:55 -0400

    adding validate_input() to validate the input data.

--- Lesson2_Problem_Sets/Days_Old.py
+++ Lesson2_Problem_Sets/Days_Old.py
@@ -6,6 +6,28 @@
 # Assume that the birthday and current date are correct dates (and no
 # time travel).
 #
+def validate_input(year1, month1, day1, year2, month2, day2):
+    args = [year1, month1, day1, year2, month2, day2]
+    i = 0
+    while i < len(args):
+        if args[i] <= 0 or type(args[1]) != int:
+            print(f"Invalid input {args[i]}, must be positive integer.")
+        i += 1
+
+    if year2 < year1:
+        print(f"Invalid input, current date must be equal or larger than birthday.")
+    elif year2 == year1:
+        if month2 < month1:
+            print(f"Invalid input, current date must be equal or larger than birthday.")
+        elif month2 == month1:
+            if day2 < day1:
+                print(f"Invalid input, current date must be equal or larger than birthday.")
+
+    if day1 > check_days_per_month(year1, month1) or day2 > check_days_per_month(year2, month2):
+        print("Incorrect input date!")
+
+    return
+
 def check_leap_year(year):
     if year % 4 == 0 and year % 100 == 0 and year % 400 != 0:
         return False
@@ -15,6 +37,7 @@ def check_leap_year(year):
         return True
     else:
         return False
+
 def check_days_per_month(year, month):
     if month in [1,3,5,7,8,10,12]:
         return 31
@@ -24,6 +47,7 @@ def check_days_per_month(year, month):
         if check_leap_year(year):
             return 29
         return 28
+
 def check_days_so_far(year, month, day):
     i = 1
     days = 0
@@ -37,6 +61,7 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     ##
     # Your code here.
     ##
+    validate_input(year1, month1, day1, year2, month2, day2)
     year = year1
     counter = 0
     while year < year2:
@@ -57,6 +82,7 @@ def test():
                   ((2011,1,1,2012,8,8), 585 ),
                   ((1900,1,1,1999,12,31), 36523)]
     for (args, answer) in test_cases:
+        assert daysBetweenDates(*args) ==answer
         result = daysBetweenDates(*args)
         if result != answer:
             print ("Test with data:", args, "failed")
