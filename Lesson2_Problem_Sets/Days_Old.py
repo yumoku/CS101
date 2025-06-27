c93f638b40fafd41c2bdb0b2803bd4c99e1e2367
yumoku <150928992+yumoku@users.noreply.github.com>
2025-06-27 10:25:04 -0400

    Using pytest and assert to improve the test codes

--- Lesson2_Problem_Sets/Days_Old.py
+++ Lesson2_Problem_Sets/Days_Old.py
@@ -76,17 +76,12 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
 # Test routine
 
 def test():
-    test_cases = [((2012,1,1,2012,2,28), 58),
+    test_cases = [((2012,1,1,2012,2,30), 58),
                   ((2012,1,1,2012,3,1), 60),
                   ((2011,6,30,2012,6,30), 366),
                   ((2011,1,1,2012,8,8), 585 ),
                   ((1900,1,1,1999,12,31), 36523)]
     for (args, answer) in test_cases:
-        assert daysBetweenDates(*args) ==answer
-        result = daysBetweenDates(*args)
-        if result != answer:
-            print ("Test with data:", args, "failed")
-        else:
-            print ("Test case passed!")
+        assert daysBetweenDates(*args) == answer
 
 test()
