cd05da8848c28f8d93ce8dabab13d9a053963eda
yumoku <150928992+yumoku@users.noreply.github.com>
2025-07-10 17:46:57 -0400

    Seperate functions into individual methods, also change from the file input to use the website url to get the contents of the html of that website.

--- ExtractLinks/ExtractLinks.py
+++ ExtractLinks/ExtractLinks.py
@@ -1,37 +1,61 @@
-def ExtractLinks(text: str):
+import requests
+
+def extract_link(text: str):
     """
     This method is to find the beginning index
     and ending index of a web url
     :param text: target text to be searched
     :return: the indexes and new string removed the web url
     """
-    target_string = "<a href=3D"
+    target_string = "<a href="
     target_sign = "\""
     anchor = text.find(target_string)
-    begining = anchor + len('<a href=3D"')
+    begining = anchor + len('<a href="')
     end = text.find(target_sign,begining + 1)
     url = text[begining:end]
 
     return url, end
-def main():
-    webpage = None
-    url = None
-    urls =[]
-    try:
-        with open("xkcd_ Good Science.mhtml","r") as file:
-            webpage = file.read()
-    except FileNotFoundError:
-        print("Error, the html file was not found.")
 
-    count = webpage.count("<a href=3D")
-    text = webpage
-    # print(f"count is {count}")
+def read_html_webpage(web_url):
+    """
+    Read the contents of a webpage and modify
+    the link url to absolute link url, also
+    adjust the protocol-relative URL
+    """
+    source = requests.get(web_url).text
+    # some links are using protocol-relative URLs
+    # such as //c.xkcd.com/random/comic/
+    # need to convert its format
+    source = source.replace('href="//c.', 'href="https://c.')
+    # some links are relative links
+    # need to convert to absolute links
+    source = source.replace('href="/', 'href="https://www.xkcd.com/')
+    return source
 
+def extract_links_from_single_webpage(web_url):
+    """
+    Given a website url as input, find all the webpage urls
+    in the html of this website√
+    """
+    extracted_urls =[]
+    source = read_html_webpage(web_url)
+    count = source.count("<a href=")
+    text = source
     for i in range(count):
-        url, end = ExtractLinks(text)
-        print(url.replace("\n",""))
-        urls.append(url)
+        url, end = extract_link(text)
+        # print(url.replace("\n",""))
+        extracted_urls.append(url)
         text = text[end:]
+
+    return extracted_urls
+
+def main():
+    webpage = "https://xkcd.com/"
+    url = None
+    urls = []
+
+    urls = extract_links_from_single_webpage(webpage)
+    print(urls)
 
 if __name__ == "__main__":
     main()
