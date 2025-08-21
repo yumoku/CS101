e09ae08001720f4e50010a450a6ef05da37ec885
yumoku <150928992+yumoku@users.noreply.github.com>
2025-08-21 19:34:11 -0400

    Completed the code and tested with http://www.xkcd.com and another website

--- crawl_web.py
+++ crawl_web.py
@@ -8,19 +8,20 @@ def crawl_web(web_url: str):
     to_crawl = extract_links_from_single_webpage(web_url)
     for item in to_crawl:
         if item not in crawled:
+            crawled.append(item)
             candidates = extract_links_from_single_webpage(item)
-            print("to_crawl is: ", to_crawl)
+
             for candidate in candidates:
                 if candidate not in crawled and candidate not in to_crawl:
+                    crawled.append(candidate)
                     to_crawl.append(candidate)
-            crawled.append(item)
 
     return crawled
 
 def main():
     all_urls = []
-    all_urls = crawl_web('https://xkcd.com/')
-    # print(all_urls)
+    all_urls = crawl_web('http://www.xkcd.com')
+    print(all_urls)
 
 if __name__ == "__main__":
     main()
--- extract_links.py
+++ extract_links.py
@@ -1,5 +1,3 @@
-from enum import verify
-
 import requests
 
 def extract_link(text: str):
@@ -15,7 +13,7 @@ def extract_link(text: str):
     begining = anchor + len('<a href="')
     end = text.find(target_sign,begining + 1)
     url = text[begining:end]
-    if url[0] == "\"":
+    if url != "" and url[0] == "\"":
         url = url[1:]
     return url, end
 
@@ -38,11 +36,28 @@ def read_html_webpage(web_url):
     source = source.replace('href="/', f'href="{web_url}')
     return source
 
+def clear_link_url_format(web_url):
+    """
+    Some urls are not HTTP or HTTPS url links,
+    or some of them have the following format which need to be cleared
+    <a href="http://www.asofterworld.com/archive.php">archives</a>...
+    """
+    formatted_web_url = ""
+    index = 0
+    if 'archive.php\'>archives' in web_url:
+        index = web_url.find('archive.php\'>archives')
+        formatted_web_url = web_url[:index + len('archive.php')]
+    else:
+        formatted_web_url = web_url
+
+    return formatted_web_url
+
 def extract_links_from_single_webpage(web_url):
     """
     Given a website url as input, find all the webpage urls
     in the html of this website√
     """
+    url_formatted =""
     extracted_urls =[]
     source = read_html_webpage(web_url)
     count = source.count("<a href=")
@@ -50,18 +65,23 @@ def extract_links_from_single_webpage(web_url):
     for i in range(count):
         url, end = extract_link(text)
         # print(url.replace("\n",""))
-        extracted_urls.append(url)
+        # The following if statement is working as a filter to filter url which don't
+        # start with http, also filter some strange format url with more than 121
+        # characters length
+        if len(url) > 4 and url[0:4] == 'http' and len(url) < 121:
+            url_formatted = clear_link_url_format(url)
+            extracted_urls.append(url_formatted)
         text = text[end:]
 
     return extracted_urls
 
 def main():
-    webpage = "https://crawler-test.com/"
+    webpage = "http://www.xkcd.com"
     url = None
     urls = []
 
     urls = extract_links_from_single_webpage(webpage)
-    # print(urls)
+    print(urls)
 
 if __name__ == "__main__":
     main()
